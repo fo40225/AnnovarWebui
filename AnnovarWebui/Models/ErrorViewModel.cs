7c6d64438453934194f144e27656fd059cbdafda
fo40225 <fo40225@hotmail.com>
2024-01-08 17:30:45 +0800

    加入專案檔案。

---
+++ AnnovarWebui/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AnnovarWebui.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
